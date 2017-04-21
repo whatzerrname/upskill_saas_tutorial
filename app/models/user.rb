f04b791ac617720b9d0200ea76f47e75898f0928
Ashley Berthon <whatzerrname@yahoo.com>
2017-04-21 11:49:41 +0000

    Added profile and user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
   belongs_to :plan
+  has_one :profile
   
   attr_accessor :stripe_card_token
   # If pro user passes validations (email, password, etc.)
